30e29724583e1adc42ac6d32447413c5b6d68624
Schneizelw <744587537@qq.com>
2020-03-14 15:00:24 +0800

    edit

--- internal/model/monitor.go
+++ internal/model/monitor.go
@@ -1,77 +1,77 @@
 package model
 
 import (
-	"time"
+    "time"
 
-	"github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus"
 )
 
 var (
-	tsQueueLengthGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "adapter",
-			Subsystem: "aggregator",
-			Name:      "queue_length",
-			Help:      "timeSeries queue length",
-		},
-		[]string{"type"},
-	)
-	mergeMetricCounter = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Namespace: "adapter",
-			Subsystem: "aggregator",
-			Name:      "merge_count",
-			Help:      "merge counter",
-		},
-		[]string{"jobname", "type"},
-	)
-	cacheDataLengthGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "adapter",
-			Subsystem: "aggregator",
-			Name:      "cache_data_length",
-			Help:      "cache data length",
-		},
-		[]string{"jobname", "type"},
-	)
-	sendRequestCounter = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Namespace: "adapter",
-			Subsystem: "aggregator",
-			Name:      "send_request_count",
-			Help:      "send request counter",
-		},
-		[]string{"succ"},
-	)
-	metricsSizeCounter = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Namespace: "adapter",
-			Subsystem: "aggregator",
-			Name:      "metrics_size",
-			Help:      "data size",
-		},
-		[]string{"jobname"},
-	)
+    tsQueueLengthGauge = prometheus.NewGaugeVec(
+        prometheus.GaugeOpts{
+            Namespace: "adapter",
+            Subsystem: "aggregator",
+            Name:      "queue_length",
+            Help:      "timeSeries queue length",
+        },
+        []string{"type"},
+    )
+    mergeMetricCounter = prometheus.NewCounterVec(
+        prometheus.CounterOpts{
+            Namespace: "adapter",
+            Subsystem: "aggregator",
+            Name:      "merge_count",
+            Help:      "merge counter",
+        },
+        []string{"jobname", "type"},
+    )
+    cacheDataLengthGauge = prometheus.NewGaugeVec(
+        prometheus.GaugeOpts{
+            Namespace: "adapter",
+            Subsystem: "aggregator",
+            Name:      "cache_data_length",
+            Help:      "cache data length",
+        },
+        []string{"jobname", "type"},
+    )
+    sendRequestCounter = prometheus.NewCounterVec(
+        prometheus.CounterOpts{
+            Namespace: "adapter",
+            Subsystem: "aggregator",
+            Name:      "send_request_count",
+            Help:      "send request counter",
+        },
+        []string{"succ"},
+    )
+    metricsSizeCounter = prometheus.NewCounterVec(
+        prometheus.CounterOpts{
+            Namespace: "adapter",
+            Subsystem: "aggregator",
+            Name:      "metrics_size",
+            Help:      "data size",
+        },
+        []string{"jobname"},
+    )
 )
 
 func InitMonitor() {
-	// Metrics have to be registered to be exposed:
-	prometheus.MustRegister(tsQueueLengthGauge)
-	prometheus.MustRegister(mergeMetricCounter)
-	prometheus.MustRegister(cacheDataLengthGauge)
-	prometheus.MustRegister(sendRequestCounter)
-	prometheus.MustRegister(metricsSizeCounter)
+    // Metrics have to be registered to be exposed:
+    prometheus.MustRegister(tsQueueLengthGauge)
+    prometheus.MustRegister(mergeMetricCounter)
+    prometheus.MustRegister(cacheDataLengthGauge)
+    prometheus.MustRegister(sendRequestCounter)
+    prometheus.MustRegister(metricsSizeCounter)
 }
 
 func GaugeMonitor() {
-	t := time.NewTicker(time.Second * time.Duration(15))
-	for {
-		<-t.C
-		tsQueueLengthGauge.With(prometheus.Labels{"type": "request"}).Set(float64(TsQueue.RequestLength()))
-		tsQueueLengthGauge.With(prometheus.Labels{"type": "merge"}).Set(float64(TsQueue.MergeLength()))
-		for jobName, agg := range Collection.whiteJobName {
-			cacheDataLengthGauge.With(prometheus.Labels{"jobname": jobName, "type": "prev"}).Set(float64(len(agg.prevCache.data)))
-			cacheDataLengthGauge.With(prometheus.Labels{"jobname": jobName, "type": "sum"}).Set(float64(len(agg.sumCache.data)))
-		}
-	}
+    t := time.NewTicker(time.Second * time.Duration(15))
+    for {
+        <-t.C
+        tsQueueLengthGauge.With(prometheus.Labels{"type": "request"}).Set(float64(TsQueue.RequestLength()))
+        tsQueueLengthGauge.With(prometheus.Labels{"type": "merge"}).Set(float64(TsQueue.MergeLength()))
+        for jobName, agg := range Collection.whiteJobName {
+            cacheDataLengthGauge.With(prometheus.Labels{"jobname": jobName, "type": "prev"}).Set(float64(len(agg.prevCache.data)))
+            cacheDataLengthGauge.With(prometheus.Labels{"jobname": jobName, "type": "sum"}).Set(float64(len(agg.sumCache.data)))
+        }
+    }
 }
--- internal/model/queue.go
+++ internal/model/queue.go
@@ -1,95 +1,95 @@
 package model
 
 import (
-	"github.com/sirupsen/logrus"
-	"github.com/prometheus/prometheus/prompb"
+    "github.com/sirupsen/logrus"
+    "github.com/prometheus/prometheus/prompb"
     "github.com/prometheus/client_golang/prometheus"
 )
 
 type TimeSeriesQueue struct {
-	requestQueue chan *prompb.TimeSeries
-	mergeQueue   chan *prompb.TimeSeries
+    requestQueue chan *prompb.TimeSeries
+    mergeQueue   chan *prompb.TimeSeries
 }
 
 var TsQueue *TimeSeriesQueue
 
 func InitQueue() {
-	buffer := 100000
-	if Conf.buffer >= buffer {
-		buffer = Conf.buffer
-	}
-	TsQueue = NewTimeSeriesQueue(buffer)
+    buffer := 100000
+    if Conf.buffer >= buffer {
+        buffer = Conf.buffer
+    }
+    TsQueue = NewTimeSeriesQueue(buffer)
 }
 
 func NewTimeSeriesQueue(buffer int) *TimeSeriesQueue {
-	return &TimeSeriesQueue{
-		requestQueue: make(chan *prompb.TimeSeries, buffer),
-		mergeQueue:   make(chan *prompb.TimeSeries, buffer),
-	}
+    return &TimeSeriesQueue{
+        requestQueue: make(chan *prompb.TimeSeries, buffer),
+        mergeQueue:   make(chan *prompb.TimeSeries, buffer),
+    }
 }
 
 func (tsq *TimeSeriesQueue) RequestProducer(wreq *prompb.WriteRequest) {
-	RunLog.WithFields(logrus.Fields{"queue length": tsq.RequestLength(), "add metrics count:": len(wreq.Timeseries)}).Info("request producer")
-	for _, ts := range wreq.Timeseries {
-		tsq.requestQueue <- ts
-		//AccLog.WithFields(logrus.Fields{"metric": GetMetric(ts) + GetSample(ts)}).Info("request producer")
-	}
+    RunLog.WithFields(logrus.Fields{"queue length": tsq.RequestLength(), "add metrics count:": len(wreq.Timeseries)}).Info("request producer")
+    for _, ts := range wreq.Timeseries {
+        tsq.requestQueue <- ts
+        //AccLog.WithFields(logrus.Fields{"metric": GetMetric(ts) + GetSample(ts)}).Info("request producer")
+    }
 }
 
 func (tsq *TimeSeriesQueue) RequestConsumer() {
-	var err error
-	var ts *prompb.TimeSeries
-	for {
-		select {
-		case ts = <-tsq.requestQueue:
-			err = Collection.MergeMetric(ts)
-			if err != nil {
-				RunLog.Error(err)
-			}
-		}
-	}
+    var err error
+    var ts *prompb.TimeSeries
+    for {
+        select {
+        case ts = <-tsq.requestQueue:
+            err = Collection.MergeMetric(ts)
+            if err != nil {
+                RunLog.Error(err)
+            }
+        }
+    }
 }
 
 func (tsq *TimeSeriesQueue) MergeProducer(ts *prompb.TimeSeries) {
-	tsq.mergeQueue <- ts
+    tsq.mergeQueue <- ts
 }
 
 func (tsq *TimeSeriesQueue) MergeConsumer() {
-	var ts *prompb.TimeSeries
-	var tsSlice []*prompb.TimeSeries
-	for {
-		select {
-		case ts = <-tsq.mergeQueue:
+    var ts *prompb.TimeSeries
+    var tsSlice []*prompb.TimeSeries
+    for {
+        select {
+        case ts = <-tsq.mergeQueue:
             // debug
-			if Conf.mode == "debug" {
-				for i, l := range ts.Labels {
-					if l.Name == "__name__" {
-						ts.Labels[i].Value += "_aggregator"
-						break
-					}
-				}
-			}
-			for _, l := range ts.Labels {
-				if l.Name == "job" {
-					metricsSizeCounter.With(prometheus.Labels{"jobname": l.Value}).Inc()
-					break
-				}
-			}
-			tsSlice = append(tsSlice, ts)
-			if len(tsSlice) == Conf.shard || len(tsq.mergeQueue) == 0 {
-				if err := client.Write(tsSlice); err != nil {
-					ReqLog.Error(err)
-				}
-				tsSlice = []*prompb.TimeSeries{}
-			}
-		}
-	}
+            if Conf.mode == "debug" {
+                for i, l := range ts.Labels {
+                    if l.Name == "__name__" {
+                        ts.Labels[i].Value += "_aggregator"
+                        break
+                    }
+                }
+            }
+            for _, l := range ts.Labels {
+                if l.Name == "job" {
+                    metricsSizeCounter.With(prometheus.Labels{"jobname": l.Value}).Inc()
+                    break
+                }
+            }
+            tsSlice = append(tsSlice, ts)
+            if len(tsSlice) == Conf.shard || len(tsq.mergeQueue) == 0 {
+                if err := client.Write(tsSlice); err != nil {
+                    ReqLog.Error(err)
+                }
+                tsSlice = []*prompb.TimeSeries{}
+            }
+        }
+    }
 }
 
 func (tsq *TimeSeriesQueue) RequestLength() int {
-	return len(tsq.requestQueue)
+    return len(tsq.requestQueue)
 }
 
 func (tsq *TimeSeriesQueue) MergeLength() int {
-	return len(tsq.mergeQueue)
+    return len(tsq.mergeQueue)
 }
